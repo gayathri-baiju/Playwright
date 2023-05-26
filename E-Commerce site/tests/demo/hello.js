91ac00e103cc10f96b440329c8140e6911827ed5
gayathribaiju <gayathrisbaiju6@gmail.com>
2023-05-26 19:12:02 +0530

    Deleted few Files

--- E-Commerce site/tests/demo/hello.js
+++
@@ -1,7 +0,0 @@
-    
-    exports.ap= function f1(){
-        return 'Hello Apple';             // export keyword -> indicates-> to import to other file
-    }
-    exports.or= function f2() {
-        return 'Hello Orange'
-    }
